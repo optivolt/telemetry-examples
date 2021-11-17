9493725aa2c18073cf992715be1fea077a04e48b
Sriram R <sriram@optivoltlabs.com>
2021-11-16 16:12:58 -0800

    added python example

---
+++ print_statistics.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+from serial import Serial
+import struct
+import time
+
+GET_DEV_INFO = b'a'
+GET_STATISTICS = b'b'
+
+SERIAL_BAUD_RATE = 115200
+SERIAL_TIMEOUT = 0.5
+
+def spin_fetch(port, request, size):
+    sn = None
+    while sn != request:
+        port.write(request)
+        sn = port.read()
+
+    if size > 1:
+        return sn + port.read(size - 1)
+    return sn
+
+class OptvController:
+    def __init__(self):
+        self._inst = None
+        self._sn = None
+
+    @property
+    def serial_number(self):
+        """returns serial number from latest connect or None"""
+        return self._sn
+
+    def connect(self, port):
+        """connect to device at serial port"""
+        if self._inst is None:
+            self._inst = Serial(port, SERIAL_BAUD_RATE, timeout=SERIAL_TIMEOUT)
+
+        # load constants into local vars
+        sn = spin_fetch(self._inst, GET_DEV_INFO, 6)
+        self._sn = struct.unpack('<LL', sn[1:-1])[0]
+
+    def disconnect(self):
+        """disconnect from device"""
+        if self._inst is not None:
+            self._inst.close()
+
+    def get_statistics(self):
+        """
+        returns a tuple containing stats
+        """
+        code = spin_fetch(self._inst, GET_STATISTICS, 1)
+
+        num_inputs = struct.unpack('<B', self._inst.read())[0]
+
+        input_voltages = struct.unpack(
+            '<' + 'L' * num_inputs,
+            self._inst.read(num_inputs * 4) # 4 bytes in 32 bits
+        )
+
+        input_currents = struct.unpack(
+            '<' + 'L' * num_inputs,
+            self._inst.read(num_inputs * 4) # 4 bytes in 32 bits
+        )
+
+        (output_volt, load_current, batt_curr) = struct.unpack(
+            '<LLL',
+            self._inst.read(12) # 4 bytes in 32 bits
+        )
+
+        self._inst.read() # clear the ;
+
+        return (
+            input_voltages,
+            input_currents,
+            output_volt,
+            load_current,
+            batt_curr,
+        )
+
+    def __del__(self):
+        self.disconnect()
+
+if __name__ == "__main__":
+    solar_panel = OptvController()
+    solar_panel.connect('/dev/cu.usbmodem144301')
+
+    print("serial number: ", solar_panel.serial_number)
+
+    try:
+        while True:
+            print(solar_panel.get_statistics())
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+
+    solar_panel.disconnect()
